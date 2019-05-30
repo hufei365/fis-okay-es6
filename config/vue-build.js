e8930b0438f306c4532c2dbef4a0a511bdda6d17
hufei <hufeispace@gmail.com>
2019-05-30 15:55:04 +0800

    new branch es6-v2 (new code for support es6)

---
+++ config/vue-build.js
@@ -0,0 +1,31 @@
+// 编译vue组件
+fis.match('*.vue', {
+    useCompile: true, // need force specific the property  // https://github.com/fex-team/fis3/commit/eadc96bfe112d5d4cf5eb349e8a2b6e195bcb5c2
+    isMod: true,
+    // useHash: true,
+    rExt: 'js',
+    useSameNameRequire: true,
+    parser: [
+        // https://github.com/ccqgithub/fis3-parser-vue-component
+        fis.plugin('vue-component', {
+            runtimeOnly: true,
+            styleNameJoin: '',
+            extractCSS: false,
+            cssScopedIdPrefix: '_v-',
+            cssScopedHashType: 'sum',
+            cssScopedHashLength: 8,
+            cssScopedFlag: '__vuec__',
+			isPartial:false,
+        })
+		,fis.plugin('babel-7') // 处理ES6
+    ]
+});
+// 编译vue组件中的sass
+fis.match('*.vue:scss', {
+    rExt: 'css',
+    parser: [
+        fis.plugin('scss', {
+            sourceMap: true,
+        })
+    ]
+});
---
+++ okay.js
@@ -0,0 +1,84 @@
+/**
+ * @author nuer
+ * @time 20151203
+ */
+
+
+var path = require('path');
+var util = require('util');
+var fis = module.exports = require('fis3');
+require('colors');
+
+fis.require.prefixes.unshift([ 'okay', 'fis-okay' ]);
+
+
+
+fis.cli.name 		  = 'okay';
+
+fis.cli.version 	  = require('./version.js');
+
+fis.cli.info 		  = fis.util.readJSON( __dirname + '/package.json' );
+
+fis.cli.help.commands = [ 'release', 'install', 'server', 'inspect' ];
+
+
+
+require('./config.js');
+
+//TODO: 实在找不到地儿放了, 因为是要配置它, 所以就靠它触发吧;
+var deployReg = /^(qa|online)$/i;
+Object.defineProperty(fis.config.data, 'map_file', {
+    get: function () {
+        var media = fis.project.currentMedia();
+
+        if ( fis.util.isString(this.value) && deployReg.test(media) ) {
+            fis.util.write(
+                path.join(fis.project.getProjectPath(), this.value),
+                ''
+            );
+        }
+        return this.value;
+    },
+    set: function ( v ) {
+        var media = fis.project.currentMedia();
+        if ( deployReg.test(media) ) {
+            fis.util.write(
+                path.join(fis.project.getProjectPath(), v),
+                util.format('module.exports = { now: %s };', Date.now())
+            );
+        }
+        this.value = v;
+    }
+});
+
+fis.match('**', {
+
+    // 跨模块 `__inline` 没有 `cache` 不能使用;
+    // parser: fis.plugin( require('./lib/standard') ),
+
+    postpackager: [
+
+        fis.plugin(require('./lib/map'), {
+            optimize: true
+        }),
+
+        // 若需处理跨模块 `script | link` 引用请将此类文件加 `isHtmlLike` 标记
+        fis.plugin( require('./lib/element') )
+
+    ]
+    
+}, -100);
+fis.match('/{views,pages}/**.{tpl,php,vm,jsp,html}', {
+
+    isHtmlLike: true,
+    extras: {
+        isPage: true
+    },
+    preprocessor: fis.plugin('extlang')
+    
+}, -100);
+fis.match('/pages/**.{vm,jsp}', {
+    
+    release: '/WEB-INF/$0'
+
+}, -100);
